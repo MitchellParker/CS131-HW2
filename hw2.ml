ab332d597976fbeca5fe15274a43bfc6647c17f8
Mitchell Parker <mitchellparker@ucla.edu>
2022-01-26 22:19:47 -0800

    Refactored make_matcher into match_nt

--- hw2.ml
+++ hw2.ml
@@ -23,30 +23,47 @@ let rec parse_tree_leaves = function
   | Leaf l -> [l]
   | Node (nt, children) -> List.flatten (List.map parse_tree_leaves children)
 
+let match_empty accept frag = accept frag
+let match_nothing accept frag = None
+
+let append_matchers matcher1 matcher2 accept =
+  matcher1 (matcher2 accept)
+
+let make_appended_matchers make_a_matcher ls =
+  let rec mams = function
+    | [] -> match_empty
+    | head::tail -> append_matchers (make_a_matcher head) (mams tail)
+  in mams ls
+
+let rec make_or_matcher make_a_matcher = function
+  | [] -> match_nothing
+  | head::tail ->
+      let head_matcher = make_a_matcher head
+      and tail_matcher = make_or_matcher make_a_matcher tail
+      in fun accept frag ->
+	   let ormatch = head_matcher accept frag
+	   in match ormatch with
+		| None -> tail_matcher accept frag
+		| _ -> ormatch
+
 let match_t t accept = function
   | [] -> None
   | hd::tl -> if t = hd then accept tl else None
 
-let make_matcher (start, prod) =
-  let rec match_helper prefix accept frag =
-    match prefix with
-      | [] -> accept frag
-      | (T x)::tl -> match_t x (match_helper tl accept) frag
-      | (N x)::tl ->
-        let rec expand = function
-          | [] -> None
-          | alt::alt_tl ->
-            let res = match_helper (alt@tl) accept frag in
-            if res = None then
-              expand alt_tl
-            else
-              res
-        in 
-        let alt_list = prod x in
-        match alt_list with
-          | [] -> match_helper tl accept frag
-          | _ -> expand alt_list
-  in match_helper [N start]
+type awksub_nonterminals =
+  | Expr | Term | Lvalue | Incrop | Binop | Num
 
+let rec match_nt prod nt =
+  let alt = prod nt in
+  if alt = [] then match_empty else
+  let match_alt =
+    let match_symbol = function
+      | T t -> match_t t
+      | N n -> match_nt prod n
+    in make_or_matcher (make_appended_matchers match_symbol)
+  in match_alt alt
+
+let make_matcher (start, prod) =
+  match_nt prod start
 
 let make_parser gram frag = None
