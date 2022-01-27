cfc2d754f7002276a86639edcf1e966aef28a2db
Mitchell Parker <mitchellparker@ucla.edu>
2022-01-26 23:10:44 -0800

    asdf

--- hw2.ml
+++ hw2.ml
@@ -23,11 +23,11 @@ let rec parse_tree_leaves = function
   | Leaf l -> [l]
   | Node (nt, children) -> List.flatten (List.map parse_tree_leaves children)
 
-let match_empty accept frag = accept frag
-let match_nothing accept frag = None
+let match_empty lz_accept frag = Lazy.force lz_accept frag
+let match_nothing lz_accept frag = None
 
-let append_matchers matcher1 matcher2 accept =
-  matcher1 (matcher2 accept)
+let append_matchers matcher1 matcher2 lz_accept =
+  matcher1 (lazy (matcher2 lz_accept))
 
 let make_appended_matchers make_a_matcher ls =
   let rec mams = function
@@ -38,32 +38,35 @@ let make_appended_matchers make_a_matcher ls =
 let rec make_or_matcher make_a_matcher = function
   | [] -> match_nothing
   | head::tail ->
-      let head_matcher = make_a_matcher head
-      and tail_matcher = make_or_matcher make_a_matcher tail
-      in fun accept frag ->
-	   let ormatch = head_matcher accept frag
-	   in match ormatch with
-		| None -> tail_matcher accept frag
-		| _ -> ormatch
+    let head_matcher = make_a_matcher head
+    and tail_matcher = make_or_matcher make_a_matcher tail
+    in fun lz_accept frag ->
+      let ormatch = head_matcher lz_accept frag
+      in match ormatch with
+        | None -> tail_matcher lz_accept frag
+        | _ -> ormatch
 
-let match_t t accept = function
+let match_t t lz_accept = function
   | [] -> None
-  | hd::tl -> if t = hd then accept tl else None
+  | hd::tl -> if t = hd then Lazy.force lz_accept tl else None
 
 type awksub_nonterminals =
   | Expr | Term | Lvalue | Incrop | Binop | Num
 
-let rec match_nt prod nt =
+let norec_make_match_nt prod mnt nt =
   let alt = prod nt in
   if alt = [] then match_empty else
   let match_alt =
     let match_symbol = function
       | T t -> match_t t
-      | N n -> match_nt prod n
+      | N n -> mnt n
     in make_or_matcher (make_appended_matchers match_symbol)
   in match_alt alt
 
-let make_matcher (start, prod) =
-  match_nt prod start
+let make_matcher (start, prod) accept =
+  let f = ref (fun nt -> assert false) in
+  let rec_match_nt = (fun nt -> norec_make_match_nt prod !f nt) in
+  f := rec_match_nt;
+  rec_match_nt start (lazy accept)
 
 let make_parser gram frag = None
