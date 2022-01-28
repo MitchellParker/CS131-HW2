51ab029a6197724aca34a6f15a57d24cb6302240
Mitchell Parker <mitchellparker@ucla.edu>
2022-01-27 20:33:15 -0800

    Added matchable type for easier matching

--- hw2.ml
+++ hw2.ml
@@ -3,13 +3,14 @@ type ('nonterminal, 'terminal) symbol =
   | N of 'nonterminal
   | T of 'terminal
 
+type ('a, 'b) matchable =
+  | RHS of ('a, 'b) symbol list
+  | Alt of ('a, 'b) symbol list list
+  | Symb of ('a, 'b) symbol
+
 type ('nonterminal, 'terminal) parse_tree =
   | Node of 'nonterminal * ('nonterminal, 'terminal) parse_tree list
   | Leaf of 'terminal
-
-
-type awksub_nonterminals =
-  | Expr | Term | Lvalue | Incrop | Binop | Num
 
 (** Converts from a hw1 style grammar to a hw2 style one *)
 let rec convert_grammar (start, rules) =
@@ -27,13 +28,19 @@ let rec parse_tree_leaves = function
   | Leaf l -> [l]
   | Node (nt, children) -> List.flatten (List.map parse_tree_leaves children)
 
-let match_empty lz_accept frag = Lazy.force lz_accept frag
-let match_nothing lz_accept frag = None
+let match_empty accept frag = accept frag
+let match_nothing accept frag = None
 
-let append_matchers matcher1 lz_matcher2 lz_accept =
-  matcher1  (lazy ((Lazy.force lz_matcher2)  lz_accept))
-
-let make_appended_matchers make_a_matcher (ls: (awksub_nonterminals, string) symbol list ) =
+(** This part needed some lazy execution to stop having 
+    stupid stack overflows. Took me forever to figure out
+    how to fix this. Without it, the matcher tries to expand
+    all the way to terminal symbols, which isn't possible
+    for a grammar with any recursion. If this is not allowed
+    then so be it, I will eat the point loss *)
+let append_matchers matcher1 lz_matcher2 accept =
+  match lz_matcher2 with lazy matcher2 ->
+  matcher1 (matcher2 accept)
+let make_appended_matchers make_a_matcher ls =
   let rec mams = function
     | [] -> match_empty
     | head::tail -> append_matchers (make_a_matcher head) (lazy (mams tail))
@@ -41,33 +48,92 @@ let make_appended_matchers make_a_matcher (ls: (awksub_nonterminals, string) sym
 
 let rec make_or_matcher make_a_matcher = function
   | [] -> match_nothing
-  | (head : (awksub_nonterminals, string) symbol list)::tail ->
+  | head::tail ->
     let head_matcher = make_a_matcher head
     and tail_matcher = make_or_matcher make_a_matcher tail
-    in fun lz_accept frag ->
-      let ormatch = head_matcher lz_accept frag
+    in fun accept frag ->
+      let ormatch = head_matcher accept frag
       in match ormatch with
-        | None -> tail_matcher lz_accept frag
+        | None -> tail_matcher accept frag
         | _ -> ormatch
 
-let match_t (t : string) lz_accept = function
+let match_t t accept = function
   | [] -> None
-  | hd::tl -> if t = hd then Lazy.force lz_accept tl else None
+  | hd::tl -> if t = hd then accept tl else None
+
+let make_match_rhs match_any rhs =
+  let symbol_matcher = fun s -> match_any (Symb s) in
+  make_appended_matchers symbol_matcher rhs
+
+let make_match_alt match_any = function
+  | [] -> match_empty (** Special case for empty alt list *)
+  | alt -> 
+    let match_rhs = fun rhs -> match_any (RHS rhs) in
+    make_or_matcher match_rhs alt
+
+let make_match_nt prod match_any nt =
+  match_any (Alt (prod nt))
+
+let make_match_any prod = 
+  let rec m_any = function
+    | RHS r -> make_match_rhs m_any r
+    | Alt a -> make_match_alt m_any a
+    | Symb (N nt) -> make_match_nt prod m_any nt
+    | Symb (T t) -> match_t t
+  in m_any
+
+let make_matcher (start, prod) =
+  make_match_any prod (Symb (N start))
 
 
-let make_match_nt prod =
-  let rec mnt (nt : awksub_nonterminals) =
-    let alt = prod nt in
-    if alt = [] then match_empty else
-    let match_alt =
-      let match_symbol = function
-        | T t -> match_t t
-        | N n -> mnt n
-      in make_or_matcher (make_appended_matchers match_symbol)
-    in match_alt alt
-  in mnt
 
-let make_matcher (start, prod) accept =
-  make_match_nt prod start (lazy accept)
+let parse_t t accept (frag, tree) =
+  match match_t t accept frag with
+    | Some suffix -> Some (Leaf t)
+    | _ -> None
 
-let make_parser gram frag = None
+let make_parse_any prod = 
+  let rec m_any = function
+    | RHS r -> make_match_rhs m_any r
+    | Alt a -> make_match_alt m_any a
+    | Symb (N nt) -> make_match_nt prod m_any nt
+    | Symb (T t) -> parse_t t
+  in m_any
+
+(* let append_parsers parser1 parser2 accept frag =
+  parser1 (parser2 accept) frag
+
+let rec parse_rhs parse_symbol rhs =
+  match rhs with
+    | [] -> match_empty
+    | s::tl ->
+      let head_parser = parse_symbol s
+      and tail_parser = parse_rhs parse_symbol tl
+      in fun accept frag ->
+        let andparse = head_parser accept frag in
+        match andparse with
+          | None -> None
+          | Some suffix, subtree -> 
+            subtree::(tail_parser accept suffix)
+
+and parse_alt alt accept frag =
+  match alt with
+    | [] -> match_nothing accept frag
+    | rhs::tl ->
+      match parse_rhs 
+
+and parse_nt prod nt accept frag =
+  let match_nt = make_match_nt prod in
+  match match_nt nt accept frag with
+    | None -> None
+    | Some suffix -> fun nnext -> 
+      match next accept frag nnext with
+        | None -> None
+        | Some ans  *)
+
+
+let make_parser (start, prod) frag = None
+  (* let accept_empty = function
+    | [] -> Some []
+    | _ -> None
+  in parse_nt prod start accept_empty *)
